e8301b5044d52b0e8e3c2c0064f6cb8c8c393365
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2021-03-29 22:22:17 -0300

    added unit test project

---
+++ Pistoladas.Tests/API/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pistoladas.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
